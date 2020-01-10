729698256f5c17e4e3a898f2f586a5ee1e7c278c
Loki <zpk.loki@gmail.com>
2020-01-10 17:58:41 +0300

    refactoring!!!!AGAIN

--- star_db/src/components/hoc-helpers/index.js
+++ star_db/src/components/hoc-helpers/index.js
@@ -1,7 +1,11 @@
 import withData from './with-data.js';
 import withSwapiService from './with-swapi-service';
+import compose from './compose'
+import withChildFunction from './with-child-function'
 
 export {
   withData,
-  withSwapiService
+  withSwapiService,
+  compose,
+  withChildFunction
 };
