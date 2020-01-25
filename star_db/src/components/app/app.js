56960b976afb0bd8a3066c0c8b8815ce3100add6
Loki <zpk.loki@gmail.com>
2020-01-25 16:40:09 +0300

    dinamic path

--- star_db/src/components/app/app.js
+++ star_db/src/components/app/app.js
@@ -7,6 +7,11 @@ import { PeoplePage, PlanetsPage, StarshipsPage } from "../pages";
 import { SwapiServiceProvider } from "../swapi-service-context/";
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import {
+  StarshipDetails,
+  PlanetDetails,
+  PersonDetails
+} from "../sw-components";
 
 export default class App extends Component {
   swapiService = new SwapiService();
@@ -34,12 +39,37 @@ export default class App extends Component {
 
             <Route
               path="/"
-              render={() => <h2 className="text-center text-danger">Welcome to StarDB</h2>}
+              render={() => (
+                <h2 className="text-center text-danger">Welcome to StarDB</h2>
+              )}
               exact={true}
             />
-            <Route path="/people" component={PeoplePage} />
-            <Route path="/planets" component={PlanetsPage} />
-            <Route path="/starships" component={StarshipsPage} />
+            <Route path="/people" component={PeoplePage} exact />
+            <Route
+              path="/people/:id"
+              render={({ match }) => {
+                const { id } = match.params;
+                return <PersonDetails itemId = {id}/>;
+              }}
+            />
+
+            <Route path="/planets" component={PlanetsPage} exact />
+            <Route
+              path="/planets/:id"
+              render={({ match }) => {
+                const { id } = match.params;
+                return <PlanetDetails itemId = {id}/>;
+              }}
+            />
+
+            <Route path="/starships" component={StarshipsPage} exact />
+            <Route
+              path="/starships/:id"
+              render={({ match }) => {
+                const { id } = match.params;
+                return <StarshipDetails itemId = {id}/>;
+              }}
+            />
           </div>
         </Router>
       </SwapiServiceProvider>
